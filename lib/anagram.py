399e0092c418e9cf7fafe0d2921907bf1ea806e3
Cristina Lynn <cristina@cristinalynn.net>
2023-09-26 12:15:17 -0700

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        match_anagram = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                match_anagram.append(word)         
+
+        return match_anagram
